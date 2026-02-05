43abb2a57acb07a844721e27be130067d6439f70
vaish091197 <vaishnavi09ks@gmail.com>
2026-02-05 18:49:02 +0000

    arrayExample.js

---
+++ arrayExample.js
@@ -0,0 +1,25 @@
+let a = [1,2,3,4,5]
+console.log("array of a :"+a)
+
+//if else
+let fact = false
+if(fact){
+    console.log("fact is true")
+}
+else{
+    console.log("fact is false")
+}
+
+//for loop
+for(i=0;i<=5;i++)
+{
+   console.log("value of i :"+i)
+}
+
+//define a object
+let bike = {
+    model :"Royal Enfield",
+    color :"Black",
+    bikenumber :"4321"
+}
+console.log("Bike Model :"+bike.model)
