e518b117ec5c0e59f2eff44e38f9a030a3be0f6f
vaish091197 <vaishnavi09ks@gmail.com>
2026-02-06 11:08:08 +0000

    added functionExample.js

--- functionExample.js
+++ functionExample.js
@@ -2,3 +2,13 @@ function sum(a, b) {
     return (a + b)
 }
 console.log("sum of numbers :" + sum(5, 10))
+
+function add(c, d) {
+    console.log(c + d)
+}
+add(10, 1)
+
+function print() {
+    console.log("hello,arun")
+}
+print()
