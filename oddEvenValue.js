f2f52953d1dca540c56aad3b0342d5ce4ab5acc4
vaish091197 <vaishnavi09ks@gmail.com>
2026-03-20 10:52:02 +0000

    oddEvenValue.js

--- oddEvenValue.js
+++ oddEvenValue.js
@@ -1,13 +1,9 @@
-let a = 10
-if(a % 2 == 0){
-    console.log("condition value is even number")
-}else{
-    console.log("condition value is odd number")
+function checkEvenorOdd(d) {
+    if (d % 2 == 0) {
+        console.log("condition value is even number")
+    } else {
+        console.log("condition value is odd number")
+    }
 }
-
-let b = 15
-if(b % 2 == 0){
-    console.log("condition value is even number")
-}else{
-    console.log("condition value is odd number")
-}
+checkEvenorOdd(7)
+checkEvenorOdd(22)
